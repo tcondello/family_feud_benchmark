0fb3cd377e75f0935bbe590d9c9b03f2bb263add
Tim C <tcondello@users.noreply.github.com>
2024-01-18 08:19:31 -0500

    next

--- process_answers.py
+++ process_answers.py
@@ -1,10 +1,14 @@
 import json
 
-f = open("responses/openAI_response_list.json")
-data = json.load(f)
-print(data)
-for res in data:
-    print(res)
+llm_response = open("responses/openAI_response_list.json")
+score_card = open("questions/question_list.json")
+res_data = json.load(llm_response)
+score_data = json.load(score_card)
+
+for question in score_data:
+    if question in res_data:
+        print(score_data[question].get("answers"))
+
 # if response from LLM in answers
 # for answer in answers:
 #     if answer.get("answer") == llm_res:
